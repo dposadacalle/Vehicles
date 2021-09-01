eb0a9f5afb1b5ab0387d0195dfcab1445c5fa9d3
Daniel Posada Calle <posadacalled@gmail.com>
2021-08-31 21:29:23 -0500

    Add seed db

--- Vehicles/Vehicles.API/Startup.cs
+++ Vehicles/Vehicles.API/Startup.cs
@@ -26,6 +26,8 @@ namespace Vehicles.API
                 x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDB>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
